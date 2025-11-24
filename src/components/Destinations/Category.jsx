8daea280bad7a78509e4295df7dfd194c7be36a7
Kashif Khan <kashifkhn6464ak@gmail.com>
2025-11-24 08:47:43 -0800

    fix cards width

--- src/components/Destinations/Category.jsx
+++ src/components/Destinations/Category.jsx
@@ -1,44 +1,3 @@
-// import React from "react";
-// import { FaPlane, FaUmbrellaBeach, FaHiking, FaShip, FaPassport, FaMountain } from "react-icons/fa";
-
-// export default function CategoriesSection() {
-//   const categories = [
-//     { icon: <FaPlane size={40} />, title: "Adventure Trips" },
-//     { icon: <FaUmbrellaBeach size={40} />, title: "Luxury Escapes" },
-//     { icon: <FaHiking size={40} />, title: "Family Vacations" },
-//     { icon: <FaShip size={40} />, title: "Honeymoon Specials" },
-//     { icon: <FaPassport size={40} />, title: "Nature Retreats" },
-//     { icon: <FaMountain size={40} />, title: "Cultural Tours" },
-//   ];
-
-//   return (
-//     <section className="py-16 bg-gray-100">
-//       <div className="text-center mb-10">
-//         <h2 class="font-poppins font-semibold md:text-[36px] text-[26px]  leading-[100%] tracking-[0.5%]"
-// >Explore by Category</h2>
-//       </div>
-
-//       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto px-4">
-//         {categories.map((cat, index) => (
-//           <div
-//             key={index}
-//             className="p-8 flex flex-col items-center 
-//                        hover:shadow-xl hover:-translate-y-1 transition-all cursor-pointer"
-//           >
-//             <div className="text-orange-500 mb-4">{cat.icon}</div>
-//             <h3 className="text-xl font-semibold">{cat.title}</h3>
-//           </div>
-//         ))}
-//       </div>
-//     </section>
-//   );
-// }
-
-
-
-
-
-
 import React from "react";
 
 // Import your images
@@ -68,11 +27,11 @@ export default function CategoriesSection() {
         </h2>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto px-22">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto md:px-22 px-4">
         {categories.map((cat, index) => (
           <div
             key={index}
-            className="p-8 flex flex-col items-center rounded-xl
+            className="p-8 flex flex-col justify-center items-center rounded-xl
                        hover:shadow-xl hover:-translate-y-1 transition-all cursor-pointer"
           >
             <img src={cat.img} alt={cat.title} className="w-36 h-36 mb-4 bg-[linear-gradient(314.35deg,#FFE4B5_14.33%,#FFD166_85.69%)] rounded-full p-5"  />
